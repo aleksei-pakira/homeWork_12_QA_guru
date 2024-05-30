42f64312e6b8e7334fc7f6f74260549bfbf8120c
alexey evlanov <lanceleelasparrow@gmail.com>
2024-05-30 13:05:40 +0400

    added keys

--- utils/attach.py
+++ utils/attach.py
@@ -1,23 +1,27 @@
+import os
 import allure
 from allure_commons.types import AttachmentType
 
+
 def add_screenshot(browser):
     png = browser.driver.get_screenshot_as_png()
-    allure.attach(body=png, name='screenshot', attachment_type=AttachmentType.PNG, extension='.png')
+    allure.attach(png, 'screenshot', AttachmentType.PNG, '.png')
+
 
 def add_logs(browser):
-    log = "".join(f'{text}\n' for text in browser.driver.get_log(log_type='browser'))
-    allure.attach(log, 'browser_logs', AttachmentType.TEXT, '.log')
+    log = ''.join(f'{text}\n' for text in browser.driver.get_log(log_type='browser'))
+    allure.attach(log, 'logs', AttachmentType.TEXT, '.log')
+
 
 def add_html(browser):
     html = browser.driver.page_source
-    allure.attach(html, 'page_source', AttachmentType.HTML, 'html')
+    allure.attach(html, 'html', AttachmentType.HTML, '.html')
 
-# скринкаст
+
 def add_video(browser):
-    video_url = f'https://selenoid.autotests.cloud/video/{browser.driver.session_id}.mp4'
+    url = os.getenv('SELENOID_URL')
+    video_url = f'https://{url}video/' + browser.driver.session_id + '.mp4'
     html = "<html><body><video width='100%' height='100%' controls autoplay><source src='" \
            + video_url \
            + "' type='video/mp4'></video></body></html>"
-    allure.attach(html, 'video_' + browser.driver.session_id, AttachmentType.HTML, '.html')
-
+    allure.attach(html, 'video', AttachmentType.HTML, '.html')
